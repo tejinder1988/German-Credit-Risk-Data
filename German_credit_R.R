7ae5474d262968cdb01e623f1651a469061a0219
Tejinder Singh <55556587+tejinder1988@users.noreply.github.com>
2019-10-28 19:26:17 +0100

    Update

--- German_credit_R.R
+++ German_credit_R.R
@@ -2,6 +2,9 @@ library(tidyr)
 library(tidyverse)
 library(ggpubr)
 library(corrplot)
+library(randomForest)
+library(cluster)
+library(fpc)
 
 
 setwd("C:/Users/Teji/Desktop/Project/German-Credit-Risk-Data")
@@ -86,25 +89,64 @@ Saving.accounts_moderate<-ifelse(data$Saving.accounts=="moderate",1,0)
 Saving.accounts_quite_rich<-ifelse(data$Saving.accounts=="quite rich",1,0)
 Saving.accounts_rich<-ifelse(data$Saving.accounts=="rich",1,0)
 
-Purpose_business<-
-  Purpose_business<-
-  Purpose_business<-
-  
+Purpose_business_business<-ifelse(data$Purpose=="business",1,0)
+Purpose_business_car<-ifelse(data$Purpose=="car",1,0)
+Purpose_business_domestic_appliances<-ifelse(data$Purpose=="domestic appliances",1,0)
+Purpose_business_education<-ifelse(data$Purpose=="education",1,0)
+Purpose_business_furniture_equipment<-ifelse(data$Purpose=="furniture/equipment",1,0)
+Purpose_business_radio_TV<-ifelse(data$Purpose=="radio/TV",1,0)
+Purpose_business_repairs<-ifelse(data$Purpose=="repairs",1,0)
+Purpose_business_vacation_others<-ifelse(data$Purpose=="vacation/others",1,0)
+
+cor_data<-cbind(cor_data,
+                sex_male,sex_female,
+                housing_free,
+                housing_own,
+                housing_rent,
+                Checking.account_little,
+                Checking.account_moderate,
+                Checking.account_rich,
+                Saving.accounts_little,
+                Saving.accounts_moderate,
+                Saving.accounts_quite_rich,
+                Saving.accounts_rich,
+                Purpose_business_business,
+                Purpose_business_car,
+                Purpose_business_domestic_appliances,
+                Purpose_business_education,
+                Purpose_business_furniture_equipment,
+                Purpose_business_radio_TV,
+                Purpose_business_repairs,
+                Purpose_business_vacation_others)
 ##
 
 
-cor_data<-data.frame(cor_data,
-                     Sex=as.numeric(data$Sex),
-                     Housing=as.numeric(data$Housing),
-                     Saving.accounts=as.numeric(data$Saving.accounts),
-                     Credit.amount=as.numeric(data$Credit.amount),
-                     Purpose=purpose)
-corrplot(cor(cor_data),method="circle")
-
-
-
+corrplot(cor(cor_data),method = "color",addCoef.col="black",number.cex= 0.5,tl.cex = 0.5,type = "upper")
 
 # Decision tree classifier
+dt_data<-data%>%
+  select(Age,Sex,Credit.amount)
 
+tot_within_ss <- sapply(1:10, function(k){
+  cl <- kmeans(dt_data[-2], k, nstart = 10)
+  cl$tot.withinss
+})
+plot(1:10, tot_within_ss, type = "b")
 
+cluster<-kmeans(data[,c(1,3)],2,nstart = 20)
+clusplot(dt_data, cluster$cluster, color=TRUE, shade=TRUE, 
+         labels=2, lines=0)
+##
+dt2_data<-data
+dt2_data<-lapply(dt2_data[,-2],as.numeric)
+dt2_data<-data.frame(dt2_data,Sex=data$Sex)
+
+tot_within_ss <- sapply(1:10, function(k){
+  cl <- kmeans(dt2_data[-9], k, nstart = 10)
+  cl$tot.withinss
+})
+plot(1:10, tot_within_ss, type = "b")
+cluster2<-kmeans(dt2_data[-9],2,nstart=20)
+clusplot(dt2_data, cluster2$cluster, color=TRUE, shade=TRUE, 
+         labels=2, lines=0)
 
